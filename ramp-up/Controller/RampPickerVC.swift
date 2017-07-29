fbb4da770f232bd2abe9db246ce939ba184f2703
Laren Sakota <jedi.temple@yahoo.com>
2017-07-28 23:06:57 -0700

    added popover and ramp images

---
+++ ramp-up/Controller/RampPickerVC.swift
@@ -0,0 +1,91 @@
+//
+//  RampPickerVC.swift
+//  ramp-up
+//
+//  Created by Laren Sakota on 7/28/17.
+//  Copyright © 2017 Laren Sakota. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+    
+    var sceneView: SCNView!
+    var size: CGSize!
+    weak var rampPlacerVC: RampPlacerVC!
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        sceneView.addGestureRecognizer(tap)
+        
+        let rotate = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(0.01 * Double.pi), z: 0, duration: 0.1))
+        
+        var obj = SCNScene(named: "art.scnassets/pipe.dae")
+        var node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
+        node?.runAction(rotate!)
+        node?.scale = SCNVector3Make(0.0022, 0.0022, 0.0022)
+        node?.position = SCNVector3Make(-1, 0.7, -1)
+        scene.rootNode.addChildNode(node!)
+        
+        obj = SCNScene(named: "art.scnassets/pyramid.dae")
+        node = obj?.rootNode.childNode(withName: "pyramid", recursively: true)!
+        node?.runAction(rotate!)
+        node?.scale = SCNVector3Make(0.0058, 0.0058, 0.0058)
+        node?.position = SCNVector3Make(-1, -0.45, -1)
+        scene.rootNode.addChildNode(node!)
+        
+        obj = SCNScene(named: "art.scnassets/quarter.dae")
+        node = obj?.rootNode.childNode(withName: "quarter", recursively: true)!
+        node?.runAction(rotate!)
+        node?.scale = SCNVector3Make(0.0058, 0.0058, 0.0058)
+        node?.position = SCNVector3Make(-1, -2.2, -1)
+        scene.rootNode.addChildNode(node!)
+        
+        preferredContentSize = size
+    }
+    
+    @objc func handleTap(_ gestureRecognizer: UIGestureRecognizer){
+        let p = gestureRecognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(p, options: [:])
+        
+        if hitResults.count > 0 {
+            let node = hitResults[0].node
+            rampPlacerVC.onRampSelected(node.name!)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
